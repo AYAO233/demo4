fc59493c4af14dc018e7cb5f671cff2bd1956a38
LinZhengyu <lzy233@126.com>
2017-10-29 16:37:56 +0800

    try to change
    try to change Github

---
+++ demo4.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo '<h1>今天天气不错，</h1>';
+    echo '<h2>挺风和日丽的。</h2>';
+
+?>
+
+<html>
+
+<head>
+    <title>一个使用CSS的简单示例</title>
+    <link rel = "StyleSheet" href = "style.css" type = "text/css" />
+
+</head>
+    
+<body>
+    <h1 style = "font-size : x-large; color : red;"> 一级标题 </h1>
+    <p class = "dark-row">Linux</p>
+    <p class = "light-row">Apache</p>
+    <p class = "dark-row">MySQL</p>
+    <p class = "light-row">PHP</p>
+    <p>试一下GitHub</p>
+</body>
+
+</html>
